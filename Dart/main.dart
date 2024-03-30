281f1a62f1980679a25d4f395874db7676e07ec5
Belmondo Rodrigues <belmondo.rodrigues@gmail.com>
2024-03-30 10:34:01 -0300

    Update main.dart

--- Dart/main.dart
+++ Dart/main.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'mape_loop.dart';
+
+void main() {
+  mape_loop mploop = new mape_loop();
+  //CHANGE THIS TO ADD THE NFRs NEEDED TO YOUR PROJECT
+  //mploop.nfr();
+  //example:
+  //mploop.nfr(new SecurityNFR());
+}
